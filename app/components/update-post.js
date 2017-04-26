158f4fc9074b65aa823a4b8c870ce7aa3c5a493e
Christian Martinez and Shane Stafford <christian+shane@>
2017-04-26 14:09:12 -0700

    update update function

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    update(post) {
+      var params = {
+        body: this.get('body'),
+        date: this.get('date'),
+        image: this.get('image'),
+        title: this.get('title')
+      };
+      this.sendAction('update', post, params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -13,5 +13,14 @@ export default Ember.Route.extend({
       newPost.save();
       this.transitionTo('index');
     }
+    // updatePost(post, params) {
+    //   Object.keys(params).forEach(function(key) {
+    //     if(params[key] !== undefined) {
+    //       post.set(key,params[key]);
+    //     }
+    //   })
+    //   post.save();
+    //   this.transitionTo('index');
+    // }
   }
 });
