35ac5fb620836bd1adc8433d67a48b5c4d913917
Christian Martinez and Shane Stafford <christian+shane@>
2017-04-26 15:13:31 -0700

    update with navbar

--- app/components/admin-mode.js
+++ app/components/admin-mode.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // isAdmin: false,
+  //
+  // actions: {
+  //   adminActivate: function() {
+  //     this.set('isAdmin', true)
+  //   }
+  // }
 });
